0101082b83834d59ad83212c3b83c4bdd0b95d59
munsohyeon <msh1787@gmail.com>
2025-05-23 13:21:20 +0900

    반응형 수정 해야함

--- public/README.md
+++ public/README.md
@@ -17,3 +17,147 @@
 ## 2. 추천
 
 - header.css 는 무조건 z-index: 999 이상
+- header 영역이 margin 의 오류로 padding 변경
+
+```
+margin-top 의 오류라고 해서 유명합니다. (웹브라우저 문제)
+이런 경우 padding-top  또는 테두리를 주어서 해결합니다.
+```
+
+- header 영역이 스크롤시 `postion:fixed 되면서 높이가 반영안됨.`
+
+```
+postion:fixed 라고 셋팅하면 높이값이 반영이 안됩니다.
+강제로 main 영역의 상단에 공간으 padding-top 으로 적용 해결.
+```
+
+- 배너 슬라이드는 단방향이라서, 즉 무한루프 가 아니므로 버튼 출력이 수정 필요.
+
+## 3. CSS 자동 정리 도구 활용
+
+- 필수 사항은 아닙니다.
+- https://h-owo-ld.tistory.com/184
+- `PostCSS Sorting` 검색 및 설치
+
+```json
+"postcssSorting.config": {
+    "order": [
+      "custom-properties",
+      "dollar-variables",
+      "at-variables",
+      {
+        "type": "at-rule",
+        "name": "extend"
+      },
+      {
+        "type": "at-rule",
+        "name": "include"
+      },
+      "declarations",
+      {
+        "type": "at-rule",
+        "name": "media"
+      },
+      "rules"
+    ],
+    "properties-order": [
+      "position",
+      "top",
+      "right",
+      "bottom",
+      "left",
+      "z-index",
+
+      "display",
+      "flex",
+      "flex-grow",
+      "flex-shrink",
+      "flex-basis",
+      "flex-direction",
+      "flex-wrap",
+      "justify-content",
+      "align-items",
+      "align-content",
+      "order",
+
+      "float",
+      "clear",
+      "box-sizing",
+      "width",
+      "min-width",
+      "max-width",
+      "height",
+      "min-height",
+      "max-height",
+      "margin",
+      "padding",
+      "overflow",
+      "overflow-x",
+      "overflow-y",
+
+      "font",
+      "font-family",
+      "font-size",
+      "font-weight",
+      "line-height",
+      "letter-spacing",
+      "text-align",
+      "text-decoration",
+      "text-transform",
+      "color",
+
+      "background",
+      "background-color",
+      "background-image",
+      "background-size",
+      "background-position",
+      "background-repeat",
+
+      "border",
+      "border-width",
+      "border-style",
+      "border-color",
+      "border-radius",
+
+      "box-shadow",
+      "opacity",
+      "transition",
+      "transform",
+
+      "cursor",
+      "visibility",
+      "content"
+    ],
+    "unspecified-properties-position": "bottom"
+  }
+```
+
+- 단축키 설정하기 : 설정 > Keyvoard Shortcuts 선택
+- 검색어로 `PostCSS Sorting Run` 선택 : Shift + Alt + P
+
+## 4. 반응형 작업
+
+### 4.1. 필수 체크 사항
+
+- 아래 구문이 없으면 화면 체크를 못해서 반응형 곤란
+
+```html
+<meta name="viewport" content="width=device, initial-scale=1.0" />
+```
+
+- 포토샵 또는 Figma 로 디자인 제공되어집(PC, 타블렛, 모바일)
+- 가능하면 큰 화면에서 모두 배치하고, 점점 줄여가면서 배치하기를 구너장
+
+```css
+/* 최대 크기 */
+@media all and (max-width: 1280px) {
+}
+@media all and (max-width: 1024px) {
+}
+@media all and (max-width: 960px) {
+}
+@media all and (max-width: 760px) {
+}
+@media all and (max-width: 540px) {
+}
+```
--- public/js/trip.js
+++ public/js/trip.js
@@ -1,3 +1,146 @@
 window.addEventListener("load", function () {
   const apiUrl = "http://127.0.0.1:5500/public/api/trip.json";
+
+  let cateFocusIndex = 0;
+  // 카테고리 button 태그 DOM 보관
+  let cateButtonArr = [];
+  // 카테고리 글자 배열
+  let cateNameArr = [];
+  // 카테고리별 목록 배열
+  let cateListArr = [];
+  // 투어 슬라이드 변수
+  let swTrip;
+
+  // api 호출 함수
+  async function getData(_url, _fn) {
+    try {
+      const res = await fetch(_url);
+      const data = await res.json();
+      _fn(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  // 데이터를 처리하는 함수
+  const parseData = function (_data) {
+    // console.log(_data);
+
+    // 1. 카테고리 글자만 추출하기
+    // const cateArr = _data.map(item => item.cate );
+    const cateArr = _data.map(function (item, index, arr) {
+      return item.cate;
+    });
+    cateNameArr = [...cateArr];
+
+    // 2. 카테고리 목록만 추출하기
+    const listArr = _data.map(function (item, index, arr) {
+      return item.list;
+    });
+    cateListArr = [...listArr];
+
+    makeTripInit();
+    activeCateFocus();
+    addBtsEvent();
+  };
+
+  // 최초 데이터가 전달되면 1 번만 실행
+  // 카테고리 버튼은 1 번만 만들어도 됨.
+  function makeTripInit() {
+    // 배치될 장소
+    const cateBts = document.querySelector(".section_category_bts");
+    // 최종 html 글자
+    let html = "";
+
+    // for (let i = 0; i < cateNameArr.length; i++) {
+    //   const tag = `<li><button>${cateNameArr[i]}</button></li>`;
+    //   html = html + tag;
+    // }
+
+    cateNameArr.forEach(function (item, index) {
+      const tag = `<li><button>${item}</button></li>`;
+      html = html + tag;
+    });
+
+    cateBts.innerHTML = html;
+
+    // 포커스 모두 제거하기
+    function removeFocusAll() {
+      cateButtonArr.forEach(function (item) {
+        item.classList.remove("cate_focus");
+      });
+    }
+
+    // 카테고리 버튼에 현재 포커스 표현하기
+    function activeCateFocus() {
+      cateButtonArr[cateFocusIndex].classList.add("cate_focus");
+      makeTripListHtml();
+    }
+
+    // 목록 html 을 만든다.
+    function makeTripListHtml() {
+      console.log("어느 목록을 출력할 것인가? " + cateListArr[cateFocusIndex]);
+      // 1. html 태그만들기
+      // 1.1. 어디다가 만들지? querySelector 찾아줌.
+      const swTripWrap = document.querySelector(".sw_trip .swiper-wrapper");
+      // 1.2. html 로 만들기
+      let html = "";
+      const listArr = cateListArr[cateFocusIndex];
+      listArr.forEach(function (item) {
+        const tag = `
+      <div class="swiper-slide">
+        <div class="item">
+          <a href="${item.link}">
+            <div class="item_image">
+              <img
+                src="${item.image}"
+                alt="${item.title}"
+                title="${item.title}"
+              />
+            </div>
+            <span class="item_name">${item.city}</span>
+            <div class="item_text">
+              <span class="item_cupon">
+              ${item.title}
+              </span>
+              <p class="item_desc">
+              ${item.content}
+              </p>
+              <span class="item_price"><b>${item.price}</b>원~</span>
+            </div>
+          </a>
+        </div>
+      </div>
+      `;
+        html = html + tag;
+      });
+      swTripWrap.innerHTML = html;
+
+      // 2. 항상 슬라이드가 만들어져 있다면 삭제하고
+      if (swTour) {
+        // swiper 를 제거합니다. (swiper 사이트의 레퍼런스 참조)
+        swTripr.destroy(true, true);
+      }
+
+      // 3. 슬라이드를 생성해야 합니다.
+      swTour = new Swiper(".sw_trip", {
+        slidesPerView: 4,
+        spaceBetween: 30,
+        speed: 2000,
+        navigation: {
+          nextEl: ".trip_next",
+          prevEl: ".trip_prev",
+        },
+        autoplay: {
+          delay: 3000,
+          disableOnInteraction: false,
+        },
+      });
+
+      // 2. swiper 생성
+    }
+
+    // 함수 호출
+    getData(apiUrl, parseData);
+  }
 });
